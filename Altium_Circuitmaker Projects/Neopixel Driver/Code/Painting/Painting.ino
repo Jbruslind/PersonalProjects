f0de3d0045d6da824abffd27028307840300ad16
Jbruslind <bruslinj@oregonstate.edu>
2021-05-14 12:03:21 -0700

    update assembly

--- Altium_Circuitmaker Projects/Neopixel Driver/Code/Painting/Painting.ino
+++ Altium_Circuitmaker Projects/Neopixel Driver/Code/Painting/Painting.ino
@@ -1,4 +1,14 @@
-#include <tinyNeoPixel.h>
+#include <Adafruit_NeoPixel.h>
+#include <Wire.h>
+// Date and time functions using a DS1307 RTC connected via I2C and Wire lib
+#include "RTClib.h"
+
+RTC_DS1307 rtc;
+
+
+#ifdef __AVR__
+ #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
+#endif
 
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN            3
@@ -14,79 +24,85 @@ struct LED_ {
   bool delay_on;
 };
 
-struct LED_ l_array [10]; 
+struct LED_ l_array [NUMPIXELS]; 
 
 unsigned long previousMillis = 0;        // will store last time LED was updated
 
-// constants won't change:
-const long interval = 1000;           // interval at which to blink (milliseconds)
 
 // When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
 // Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
 // example for more information on possible values.
-tinyNeoPixel pixels = tinyNeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
 
 void setup() {
   // put your setup code here, to run once:
+  
+  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
+    clock_prescale_set(clock_div_1);
+  #endif
 
   pixels.begin(); // This initializes the NeoPixel library.
   pixels.show();
+  pixels.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
   randomSeed(analogRead(0));
 
-  pixels.setPixelColor(0, pixels.Color(255,255,255));
-  pixels.setPixelColor(1, pixels.Color(255,255,255));
-  pixels.setPixelColor(2, pixels.Color(255,255,255));
-  pixels.setPixelColor(3, pixels.Color(50,0, 255));
-  pixels.setPixelColor(4, pixels.Color(255,255,255));
-  pixels.setPixelColor(5, pixels.Color(255,255,255));
-  pixels.setPixelColor(6, pixels.Color(255,255,255));
-  pixels.setPixelColor(7, pixels.Color(255,255,255));
-  pixels.setPixelColor(8, pixels.Color(255,255,255));
-  pixels.setPixelColor(9, pixels.Color(255,255,255));
-  
-  for(int i = 0; i < 10; i++)
+  for(int i = 0; i < NUMPIXELS; i++)
   {
-    l_array[i].delay_t = random(500, 4000);
-    l_array[i].fade_t = random(500, 1000);
-    l_array[i].prev_t = millis();
-    l_array[i].brightness = 0;
-    l_array[i].delay_on = true;
+    pixels.setPixelColor(i, pixels.Color(255,255,255));
+    pixels.show();
   }
+  
+
+  if (! rtc.begin()) {
+    //Serial.println("Couldn't find RTC");
+    //Serial.flush();
+    pixels.setPixelColor(0, pixels.Color(255,0,0)); // red
+    pixels.show();
+    //abort();
+  }
+  pixels.setPixelColor(0, pixels.Color(0,255,0)); // Green
+  pixels.show();
+  if (! rtc.isrunning()) {
+    pixels.setPixelColor(0, pixels.Color(255,255,0)); // Green
+    pixels.show();
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+
 }
 
 void loop() {
+//
+//  unsigned long currentMillis = millis();
+//  
+//  for(int i = 0; i < NUMPIXELS; i++)
+//  {
+//    
+//    pixels.setPixelColor(i, pixels.Color(l_array[i].brightness, l_array[i].brightness, l_array[i].brightness));
+//  }
+//  pixels.show();
 
-  unsigned long currentMillis = millis();
-  
-  for(int i = 0; i < 10; i++)
+  DateTime now = rtc.now();
+
+  if(now.hour() == '5')
   {
-    if(currentMillis - l_array[i].prev_t <= l_array[i].delay_t && l_array[i].delay_on == true)
-    {
-      l_array[i].brightness = 0; 
-      l_array[i].delay_on = false;
-      l_array[i].prev_t = millis(); //reset counter for the fade timer
-    }
-    else if(currentMillis - l_array[i].prev_t >= l_array[i].fade_t && l_array[i].delay_on == false)
-    {
-      LED_Done(l_array[i]);
-    }
-    else if(l_array[i].delay_on == false)
-    {
-      unsigned long temp = currentMillis - l_array[i].prev_t; 
-      float val = temp / l_array[i].fade_t;
-
-      if(val >= 0.5)
-      {
-        l_array[i].brightness -= 255 / l_array[i].fade_t; 
-      }
-      else
-      {
-        l_array[i].brightness -= 255 / l_array[i].fade_t; 
-      }
-    }
-    pixels.setPixelColor(i, pixels.Color(l_array[i].brightness, l_array[i].brightness, l_array[i].brightness));
+    pixels.setPixelColor(0, pixels.Color(255,0,255)); // Pink
   }
+  
   pixels.show();
+  delay(1000);
 }
 
 void LED_Done(struct LED_ temp)
